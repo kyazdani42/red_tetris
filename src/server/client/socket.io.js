3888d92e011fc3dac903ea75f4714ee6e2cfcdaa
vguillem <v.guillemer@gmail.com>
2019-01-18 22:08:12 +0100

    socket

--- src/server/client/socket.io.js
+++ src/server/client/socket.io.js
@@ -1,8 +1,6 @@
 const	socket = require('socket.io');
 const uniqid = require('uniqid');
 
-const config = require('../config');
-
 let io;
 const rooms = [];
 
@@ -17,18 +15,48 @@ const initServer = (server) => {
   });
 };
 
-const getRooms = () => rooms;
+const getRooms = () => rooms.reduce((accu, value, index) => {
+  return accu.push(index);
+}, []);
 
 const getSocket = () => socket;
 
 const newRoom = () => {
   // const name = uniqid();
   const name = '123';
-  rooms[name] = {};
+  rooms[name] = {
+    data: {
+      running: false,
+      playeurs: [],
+      owner: null,
+    },
+  };
   rooms[name].socket = io
     .of(`/${name}`)
     .on('connection', (socket) => {
+      if (rooms[name].data.running) {
+        socket.disconnect();
+      }
+
+      rooms[name].data.playeurs.push({id: socket.id, playing: false});
+      if (!rooms[name].data.owner) {
+        rooms[name].data.owner = socket.id;
+      }
       console.log(socket.id);
+
+      socket.on('disconnect', () => {
+        const playeurIndex = rooms[name].data.playeurs.findIndex((playeur) => {
+          return playeur.id === socket.id;
+        });
+        rooms.data.playeurs.splice(playeurIndex, 1);
+        if (rooms.data.playeurs !== {}) {
+          if (rooms[name].data.owner === socket.id) {
+            rooms[name].data.owner = rooms[name].data.playeurs[0].id;
+          }
+        } else {
+          rooms[name] = undefined;
+        }
+      });
 
       socket.on('update', (data) => {
         console.log('update');
