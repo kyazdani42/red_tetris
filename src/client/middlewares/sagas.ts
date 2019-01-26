daae9f2939f8d68c0929f278c0c1453bfbffb11a
kyazdani42 <kyazdani@student.42.fr>
2019-01-26 13:54:02 +0100

    renablesaga

--- src/client/middlewares/sagas.ts
+++ src/client/middlewares/sagas.ts
@@ -1,9 +1,9 @@
 import { all, call, put, take } from 'redux-saga/effects';
-// import * as io from 'socket.io-client';
+import * as io from 'socket.io-client';
 
 import { setSocket } from '../actions/actions';
 import { CHANGE_PAGE, CREATE_ROOM } from '../actions/constants';
-// import { request } from './utils';
+import { request } from './utils';
 
 export default function* rootSaga() {
   do {
@@ -15,9 +15,8 @@ export default function* rootSaga() {
 
 function* createRoomSaga() {
   while (yield take(CREATE_ROOM)) {
-    // const data = yield request('/room', 'POST');
-    // const socket: SocketIOClient.Socket = io(`http://localhost:3000/${data.newRoomName}`);
-    const socket = null;
+    const data = yield request('/room', 'POST');
+    const socket: SocketIOClient.Socket = io(`http://localhost:3000/${data.newRoomName}`);
     yield put(setSocket(socket));
   }
 }
