a964ef239fb77a2076b805b50454c4b21322f6ff
vguillem <v.guillemer@gmail.com>
2019-02-03 20:57:19 +0100

    'backOp'

--- src/server/models/Player.js
+++ src/server/models/Player.js
@@ -1,4 +1,4 @@
-const { isInContactWithStack, fusionPieceAndStack, checkPosition, initStack } = require('../utils/player');
+const { fusionPieceAndStack, checkPosition, initStack, updateFullLine, addFixLine } = require('../utils/player');
 const Piece = require('./Piece');
 
 class Player {
@@ -8,6 +8,7 @@ class Player {
     this.pieceIndex = 0;
     this.stack = initStack();
     this.piece = null;
+    this.isPlaying = false;
   }
 
   setNextPiece(pieceData) {
@@ -21,21 +22,67 @@ class Player {
   }
 
   updateStack() {
-    console.log(this.stack);
-    if (isInContactWithStack(this.piece, this.stack)) {
-      console.log('ici');
+    let {x, y, pattern} = this.piece;
+    let nbLine = 0;
+    if (checkPosition(x, y + 1, pattern, this.stack)) {
+      console.log('pose');
+      this.isPlaying = !checkPosition(x, y, pattern, this.stack);
+      console.log(this.isPlaying);
       this.stack = fusionPieceAndStack(this.piece, this.stack);
+      console.log('fusion');
+      pattern.forEach((line, patternY) => {
+        if (y + patternY < 20 && updateFullLine(y + patternY, this.stack)) {
+          console.log('line !');
+          nbLine++;
+        }
+      });
       this.piece.fixPiece();
     }
+    return nbLine;
   };
 
   tryMoveDown() {
     let { x, y, pattern } = this.piece;
-    if (checkPosition(x, y + 1, pattern, this.stack)) {
+    if (!checkPosition(x, y + 1, pattern, this.stack)) {
       this.piece.moveDown();
     }
   };
 
+  tryRotate() {
+    let { x, y, patternIndex, patterns } = this.piece;
+    if (!checkPosition(x, y, patterns[(patternIndex + 1) % 4], this.stack)) {
+      this.piece.rotate();
+    }
+  };
+
+  tryMoveRight() {
+    let { x, y, pattern } = this.piece;
+    if (!checkPosition(x + 1, y, pattern, this.stack)) {
+      this.piece.moveRight();
+    }
+  };
+
+  tryMoveLeft() {
+    let { x, y, pattern } = this.piece;
+    if (!checkPosition(x - 1, y, pattern, this.stack)) {
+      this.piece.moveLeft();
+    }
+  };
+
+  tryGoDown() {
+    let { x, pattern } = this.piece;
+    while (!checkPosition(x, this.piece.y + 1, pattern, this.stack)) {
+      this.piece.moveDown();
+    }
+  };
+
+  addLine(nbLine) {
+    for (let add = 0; add < nbLine; add++) {
+      if (!addFixLine(this.stack)) {
+        this.isPlaying = false;
+      }
+    }
+  };
 }
 
 module.exports = Player;
